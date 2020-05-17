30d17b0634915c19dd752658c524a7d91337ccf8
João Coimbra <joao.benatti.coimbra@gmail.com>
2020-05-17 15:19:57 -0300

    colocando janela pop up das músicas

--- public/script.js
+++ public/script.js
@@ -2,12 +2,13 @@
 var audio;
 
 //Página carregada
-function onLoadPage() {
-    //Acessar controle Audio via JavaScript
+
+setInterval(loadAudio(), 1000);
+
+function loadAudio(){
     audio = document.getElementById("audio");
 
     audio.addEventListener("play", onPlayMusic, false);
-    audio.addEventListener("timeupdate", onAtualizarTempo, false);
 }
 
 //Controlar Volume
@@ -61,4 +62,123 @@ function secToStr( seg ) {
     if (segundos < 10) segundos = "0"+segundos;
 
     return horas+":"+minutos+":"+segundos;
+}
+
+function openAlbum(num) {
+
+    document.querySelector(".wrap").style.display = "block";
+    document.querySelector("nav").style.position = "relative";
+    document.querySelector("main#first-page").style.marginTop = "20px";
+
+    switch (num) {
+        case 1:
+            document.getElementById("albumname").innerHTML = "Rock Nacional";
+            document.getElementById("p1").innerHTML = "O dia que não terminou";
+            document.getElementById("p2").innerHTML = "Olhos certos";
+            document.getElementById("p3").innerHTML = "Outro lugar";
+            document.getElementById("p4").innerHTML = "Um cara de sorte";
+            break;
+
+        case 2:
+            document.getElementById("albumname").innerHTML = "Hit Nacional";
+            document.getElementById("p1").innerHTML = "O dia que não terminou";
+            document.getElementById("p2").innerHTML = "Olhos certos";
+            document.getElementById("p3").innerHTML = "Outro lugar";
+            document.getElementById("p4").innerHTML = "Um cara de sorte";
+            break;
+        
+        case 3:
+            document.getElementById("albumname").innerHTML = "Rock";
+            document.getElementById("p1").innerHTML = "Every you";
+            document.getElementById("p2").innerHTML = "Music name";
+            document.getElementById("p3").innerHTML = "Music name";
+            document.getElementById("p4").innerHTML = "Music name";
+            break;
+        default:
+            resetNames();
+    }
+}
+
+function closePopUp() {
+    document.querySelector(".wrap").style.display = "none";
+    document.querySelector("nav").style.position = "fixed";
+    document.querySelector("main#first-page").style.marginTop = "100px";
+}
+
+function resetNames() {
+    document.getElementById("albumname").innerHTML = "Album name";
+    document.getElementById("p1").innerHTML = "Music name";
+    document.getElementById("p2").innerHTML = "Music name";
+    document.getElementById("p3").innerHTML = "Music name";
+    document.getElementById("p4").innerHTML = "Music name";
+}
+
+function playMusic(num) {
+
+    document.querySelector(".wrap").style.display = "none";
+    document.querySelector("main#audio-player").style.display = "block";
+    document.querySelector("main#first-page").style.display = "none";
+    document.querySelector(".navbar h1").style.display = "none";
+    document.querySelector("#arrow-left").style.display = "block";
+    document.querySelector("nav").style.position = "absolute";
+
+    let albumName = document.querySelector("#albumname").innerHTML;
+    let aud = document.querySelector("source#player");
+
+    switch (albumName) {
+        case "Rock Nacional":
+            switch (num) {
+                case 1:
+                    break;
+                
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                default:
+                    break;
+                }
+            break;
+            
+        case "Rock":
+            switch (num) {
+                case 1:
+                    aud.src = 'public/audio/every_you.mp3';
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                default:
+                    break;
+            }
+            break;
+        default:
+            break;
+        }
+
+}
+
+function returnFirstPage() {
+    document.querySelector("main#first-page").style.display = "block";
+    document.querySelector("main#audio-player").style.display = "none";
+    document.querySelector(".navbar h1").style.display = "block";
+    document.querySelector("#arrow-left").style.display = "none";
+    document.querySelector("main").style.marginTop = "100px";
+    document.querySelector("nav").style.position = "fixed";
+
+    resetNames();
+    let audio = document.querySelector("source#player");
+    audio.src = "";
 }
